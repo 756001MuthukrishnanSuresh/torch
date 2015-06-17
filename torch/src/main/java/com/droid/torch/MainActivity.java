ea14a889c36739c14c35df0d763a151edeced1ef
Muthukrishnan Suresh <muthukrishnan.suresh@tcs.com>
2015-06-17 12:31:19 +0530

    muthu - local commit 1

--- torch/src/main/java/com/droid/torch/MainActivity.java
+++ torch/src/main/java/com/droid/torch/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends ActionBarActivity  {
 
         //missing HW dialog
         // muthu - server commit
+        // muthu - local commit
         AlertDialog openSettingsDlg = new AlertDialog.Builder(this).
                 setIcon(R.drawable.warning).
                 setTitle(R.string.dlg_title).
